7e7f5a074be1a0bf0b28b29b4b4827fde23da948
martinezcoder <martinezcoder@gmail.com>
2017-01-21 17:38:02 +0100

    checking all except diagonal

--- main.rb
+++ main.rb
@@ -1,24 +1,82 @@
 require 'pry'
 
-RANGE = 0..2
+class Board
+  class OutOfBoard < StandardError; end
 
-board = Hash.new("_")
+  attr_reader :board
+
+  MAX = 2
+
+  def initialize
+    @board = Hash.new("_")
+  end
+
+  def [](position)
+    @board[[position.x, position.y]]
+  end
+
+  def set(player, position)
+    self[position] = player
+    return true if winner?(player, position)
+    false
+  end
+
+  private
+
+  def []=(coord, player)
+    raise OutOfBoard unless coord.valid_position?(MAX)
+    @board[[coord.x, coord.y]] = player
+  end
+
+  def winner?(player, coord)
+    return true if x_axis(coord.x) == player*(MAX+1)
+    return true if y_axis(coord.y) == player*(MAX+1)
+    false
+  end
+
+  def x_axis(x)
+    [x].product((0..MAX).to_a).collect { |index| board[index] }.join
+  end
+
+  def y_axis(y)
+    (0..MAX).to_a.product([y]).collect { |index| board[index] }.join
+  end
+end
+
+class Position
+  attr_reader :x, :y
+  def initialize(x, y)
+    @x = x.to_i
+    @y = y.to_i
+  end
+
+  def valid_position?(max)
+    x <= max && y <= max
+  end
+
+  def is_corner?
+    (x == 0 && y == 0) || x == 2 || y == 2
+  end
+end
+
+board = Board.new
 
 begin
-  puts "Insert coordinate (e.g., '1,1'):"
-  input = gets.chomp.downcase
-  coord = /([0-2]),\s?([0-2])/.match(input)
-end while coord.nil?
+  begin
+    puts "Insert coordinate (e.g., '1,1'):"
+    input = gets.chomp.downcase
+    coord = /([0-2]),\s?([0-2])/.match(input)
+  end while coord.nil?
 
-x = coord[1].to_i
-y = coord[2].to_i
+  position = Position.new(coord[1], coord[2])
 
-board[[x, y]] = "X"
+  begin
+    winner = board.set("X", position)
+    puts "X wins!" if winner
+  rescue Board::OutOfBoard => e
+    puts "#{e}. Please, enter a valid coordinate"
+  end
 
-binding.pry
-
-[x].product((RANGE).to_a).collect { |index| board[index] }
-(RANGE).to_a.product([y]).collect { |index| board[index] }
-
+end until winner
 
 
