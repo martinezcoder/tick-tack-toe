2825840a0ab4d537bf1ebad08f69c3caf2522a2c
martinezcoder <martinezcoder@gmail.com>
2017-01-22 14:25:30 +0100

    validate positions

--- main.rb
+++ main.rb
@@ -21,41 +21,57 @@ class Board
     false
   end
 
+  def draw
+    puts "    0 1 2  "
+    puts "  #########"
+    puts "0 # #{x_axis(0).join(":")} #"
+    puts "1 # #{x_axis(1).join(":")} #"
+    puts "2 # #{x_axis(2).join(":")} #"
+    puts "  #########"
+    puts
+  end
+
+  def valid_position?(position)
+    position.x <= MAX && position.y <= MAX && board[position] == "_"
+  end
+
   private
 
-  def []=(coord, player)
-    raise OutOfBoard unless coord.valid_position?(MAX)
-    @board[[coord.x, coord.y]] = player
-  end
-
-  def winner?(player, coord)
-    return true if x_axis(coord.x) == player*(MAX+1)
-    return true if y_axis(coord.y) == player*(MAX+1)
-    false
-  end
-
   def x_axis(x)
-    [x].product((0..MAX).to_a).collect { |index| board[index] }.join
+    [x].product((0..MAX).to_a).collect { |index| board[index] }
   end
 
   def y_axis(y)
-    (0..MAX).to_a.product([y]).collect { |index| board[index] }.join
+    (0..MAX).to_a.product([y]).collect { |index| board[index] }
+  end
+
+  def []=(position, player)
+    raise OutOfBoard unless valid_position?(position)
+    @board[[position.x, position.y]] = player
+  end
+
+  def in_diagonal?(position)
+    position.x == position.y || position.x == 2 || position.y == 2
+  end
+
+  def winner?(player, coord)
+    return true if x_axis(coord.x).join == player*(MAX+1)
+    return true if y_axis(coord.y).join == player*(MAX+1)
+    false
   end
 end
 
 class Position
   attr_reader :x, :y
-  def initialize(x, y)
-    @x = x.to_i
-    @y = y.to_i
+  def initialize(coord)
+    if coord
+      @x = coord[1].to_i
+      @y = coord[2].to_i
+    end
   end
 
-  def valid_position?(max)
-    x <= max && y <= max
-  end
-
-  def is_corner?
-    (x == 0 && y == 0) || x == 2 || y == 2
+  def nil?
+    x.nil? || y.nil?
   end
 end
 
@@ -63,20 +79,21 @@ board = Board.new
 
 begin
   begin
-    puts "Insert coordinate (e.g., '1,1'):"
+    board.draw
+    puts "Insert a valid coordinate (e.g., '1,1'):"
     input = gets.chomp.downcase
-    coord = /([0-2]),\s?([0-2])/.match(input)
-  end while coord.nil?
-
-  position = Position.new(coord[1], coord[2])
+    #coord = /([0-2]),\s?([0-2])/.match(input)
+    position = Position.new(/([0-2]),\s?([0-2])/.match(input))
+  end while position.nil? || !board.valid_position?(position)
 
   begin
     winner = board.set("X", position)
-    puts "X wins!" if winner
+    if winner
+      board.draw
+      puts "X wins!"
+    end
   rescue Board::OutOfBoard => e
     puts "#{e}. Please, enter a valid coordinate"
   end
-
 end until winner
-
 
